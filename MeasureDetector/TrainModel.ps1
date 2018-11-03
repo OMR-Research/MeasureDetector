7076d595754b815a316a8afe52d9d88c86d9aa77
Alexander Pacha <alexander.pacha@gmail.com>
2018-11-03 17:07:57 +0100

    Adding next configuration

--- MeasureDetector/TrainModel.ps1
+++ MeasureDetector/TrainModel.ps1
@@ -11,19 +11,19 @@ cd $pathToGitRoot
 # Available configurations - uncomment the one to actually run #
 ################################################################
 $configuration = "ssd_resnet50_v1_fpn_shared_box_predictor_640x640_edirom"
-#$configuration = "faster_rcnn_inception_v2_edirom"
+$configuration = "faster_rcnn_inception_v2_edirom"
 
 echo "Training with $($configuration) configuration"
 
-# # Legacy slim-based
-# Start-Transcript -path "$($pathToTranscript)/Train-$($configuration).txt" -append
-# python research/object_detection/legacy/train.py --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToData)/checkpoints-$($configuration)-train"
-# Stop-Transcript
-
-# Estimator-based
-Start-Transcript -path "$($pathToTranscript)/TrainEval-$($configuration).txt" -append
-python research/object_detection/model_main.py --alsologtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --model_dir="$($pathToData)/$($configuration)"
+# Legacy slim-based
+Start-Transcript -path "$($pathToTranscript)/Train-$($configuration).txt" -append
+python research/object_detection/legacy/train.py --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToData)/checkpoints-$($configuration)-train"
 Stop-Transcript
+
+# # Estimator-based
+# Start-Transcript -path "$($pathToTranscript)/TrainEval-$($configuration).txt" -append
+# python research/object_detection/model_main.py --alsologtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --model_dir="$($pathToData)/$($configuration)"
+# Stop-Transcript
 
 
 # C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\training_scripts\ValidateModel.ps1
