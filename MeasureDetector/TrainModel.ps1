369ae8d23b376269dc95d5003ac11f5b75bebb87
Alexander Pacha <alexander.pacha@gmail.com>
2019-03-15 14:55:59 +0100

    Updating run configurations

--- MeasureDetector/TrainModel.ps1
+++ MeasureDetector/TrainModel.ps1
@@ -4,8 +4,8 @@ $pathToTranscript = "$($pathToSourceRoot)/transcripts"
 $pathToData = "$($pathToSourceRoot)/data"
 cd $pathToGitRoot
 
-#echo "Appending required paths to temporary PYTHONPATH"
-#$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
+echo "Appending required paths to temporary PYTHONPATH"
+$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
 
 ################################################################
 # Available configurations - uncomment the one to actually run #
@@ -14,19 +14,17 @@ cd $pathToGitRoot
 #$configuration = "ssd_retinanet_edirom_no_transfer_learning"
 #$configuration = "faster_rcnn_inception_v2_edirom"
 #$configuration = "faster_rcnn_inception_resnet_v2_atrous_edirom"
-$configuration = "faster_rcnn_inception_v2_muscima_pp"
+#$configuration = "faster_rcnn_inception_v2_muscima_pp"
+$configuration = "faster_rcnn_inception_resnet_v2_atrous_muscima_pp"
 
 echo "Training with $($configuration) configuration"
 
 # Legacy slim-based
 Start-Transcript -path "$($pathToTranscript)/Train-$($configuration).txt" -append
-python research/object_detection/legacy/train.py --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToData)/checkpoints-$($configuration)-train"
+python research/object_detection/legacy/train.py --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --train_dir="$($pathToData)/$($configuration)"
 Stop-Transcript
 
 # # Estimator-based
 # Start-Transcript -path "$($pathToTranscript)/TrainEval-$($configuration).txt" -append
 # python research/object_detection/model_main.py --alsologtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --model_dir="$($pathToData)/checkpoints-$($configuration)"
 # Stop-Transcript
-
-
-# C:\Users\Alex\Repositories\MusicObjectDetector-TF\MusicObjectDetector\training_scripts\ValidateModel.ps1
--- MeasureDetector/ValidateModel.ps1
+++ MeasureDetector/ValidateModel.ps1
@@ -4,17 +4,18 @@ $pathToTranscript = "$($pathToSourceRoot)/transcripts"
 $pathToData = "$($pathToSourceRoot)/data"
 cd $pathToGitRoot
 
-#echo "Appending required paths to temporary PYTHONPATH"
-#$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
+echo "Appending required paths to temporary PYTHONPATH"
+$env:PYTHONPATH = "$($pathToGitRoot);$($pathToGitRoot)/research;$($pathToGitRoot)/research/slim;$($pathToSourceRoot)"
 
 ################################################################
 # Available configurations - uncomment the one to actually run #
 ################################################################
-$configuration = "ssd_resnet50_v1_fpn_shared_box_predictor_640x640_edirom"
-$configuration = "faster_rcnn_inception_v2_edirom"
+# $configuration = "ssd_resnet50_v1_fpn_shared_box_predictor_640x640_edirom"
+# $configuration = "faster_rcnn_inception_v2_edirom"
+$configuration = "faster_rcnn_inception_resnet_v2_atrous_muscima_pp"
 
 echo "Validate with $($configuration) configuration"
 
 Start-Transcript -path "$($pathToTranscript)/Validate-$($configuration).txt" -append
-python research/object_detection/legacy/eval.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --checkpoint_dir="$($pathToData)/checkpoints-$($configuration)-train" --eval_dir="$($pathToData)/checkpoints-$($configuration)-validate"
+python research/object_detection/legacy/eval.py --logtostderr --pipeline_config_path="$($pathToSourceRoot)/configurations/$($configuration).config" --checkpoint_dir="$($pathToData)/$($configuration)" --eval_dir="$($pathToData)/$($configuration)/eval"
 Stop-Transcript
