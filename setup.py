2727128f241c2fcc46032aef8b583ce8c6e9214b
Alexander Pacha <alexander.pacha@gmail.com>
2019-04-24 11:00:19 +0200

    Adding setup.py to root, for allowing for linking the sources and ease running of python code.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from __future__ import print_function, unicode_literals
+from distutils.core import setup
+
+setup(
+    name='MeasureDetector',
+    version='0.1',
+    packages=['MeasureDetector', 'MeasureDetector.configurations', 'MeasureDetector.demo'],
+    url='https://github.com/omr-research/measuredetector',
+    license='MIT',
+    author='Alexander Pacha, Simon Waloschek',
+    author_email='alexander.pacha@tuwien.at, s.waloschek@cemfi.de',
+    description='A deep-learning based detector for measures in musical scores built on top of the Tensorflow Object Detection API'
+)
