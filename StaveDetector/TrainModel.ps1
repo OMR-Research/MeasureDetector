124ee4c8760dbec7f2ce32086ba08111cc4d6c8a
Alexander Pacha <alexander.pacha@gmail.com>
2019-09-19 10:22:49 +0200

    Playing with other configurations

--- StaveDetector/TrainModel.ps1
+++ StaveDetector/TrainModel.ps1
@@ -10,6 +10,7 @@ cd $pathToGitRoot
 ################################################################
 $configuration = "faster_rcnn_inception_resnet_v2_atrous_fine_grid_staves"
 $configuration = "mask_rcnn_inception_resnet_v2_atrous_muscima-pp_staves"
+$configuration = "mask_rcnn_resnet50_muscima-pp_staves"
 
 echo "Training with $($configuration) configuration"
 
@@ -18,9 +19,9 @@ Start-Transcript -path "$($pathToTranscript)/Train-$($configuration).txt" -appen
 python research/object_detection/legacy/train.py --pipeline_config_path="$($pathToConfigurations)/$($configuration).config" --train_dir="$($pathToData)/$($configuration)"
 Stop-Transcript
 
-Start-Transcript -path "$($pathToTranscript)/Validate-$($configuration).txt" -append
-python research/object_detection/legacy/eval.py --logtostderr --pipeline_config_path="$($pathToConfigurations)/$($configuration).config" --checkpoint_dir="$($pathToData)/$($configuration)" --eval_dir="$($pathToData)/$($configuration)/eval"
-Stop-Transcript
+#Start-Transcript -path "$($pathToTranscript)/Validate-$($configuration).txt" -append
+#python research/object_detection/legacy/eval.py --logtostderr --pipeline_config_path="$($pathToConfigurations)/$($configuration).config" --checkpoint_dir="$($pathToData)/$($configuration)" --eval_dir="$($pathToData)/$($configuration)/eval"
+#Stop-Transcript
 
 # # Estimator-based
 # Start-Transcript -path "$($pathToTranscript)/TrainEval-$($configuration).txt" -append
